140702f708138bf89168bb6fffad022424606641
rodriguesporan <rodriguesporan@gmail.com>
2021-02-26 15:18:15 -0300

    feat: added implicit intent

--- app/src/main/java/com/example/wordsapp/DetailActivity.kt
+++ app/src/main/java/com/example/wordsapp/DetailActivity.kt
@@ -25,6 +25,7 @@ import com.example.wordsapp.databinding.ActivityDetailBinding
 class DetailActivity : AppCompatActivity() {
     companion object {
         val LETTER = "letter"
+        val SEARCH_PREFIX = "https://www.google.com/search?q="
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
